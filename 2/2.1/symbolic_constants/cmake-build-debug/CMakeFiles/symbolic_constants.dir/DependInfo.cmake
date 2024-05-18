34f0fe4cb011bd6a95ca6da5d6bd23ae9e03e29a
Doub1e007 <3477848352@qq.com>
2024-05-18 14:48:53 +0800

    Initial commit

---
+++ 2/2.1/symbolic_constants/cmake-build-debug/CMakeFiles/symbolic_constants.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/学习资料/408/数据结构/01.24C语言督学训练营/04.课件，代码合集/Code/2/2.1/2-float/main.c" "CMakeFiles/symbolic_constants.dir/40cab3f567a6d3876ff41bf5ebbb9412/Code/2/2.1/2-float/main.c.obj" "gcc" "CMakeFiles/symbolic_constants.dir/40cab3f567a6d3876ff41bf5ebbb9412/Code/2/2.1/2-float/main.c.obj.d"
+  "D:/学习资料/408/数据结构/01.24C语言督学训练营/04.课件，代码合集/Code/2/2.1/symbolic_constants/main.c" "CMakeFiles/symbolic_constants.dir/main.c.obj" "gcc" "CMakeFiles/symbolic_constants.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2/2.1/symbolic_constants/cmake-build-debug/CMakeFiles/symbolic_constants.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/symbolic_constants.dir/40cab3f567a6d3876ff41bf5ebbb9412/Code/2/2.1/2-float/main.c.obj"
+  "CMakeFiles/symbolic_constants.dir/40cab3f567a6d3876ff41bf5ebbb9412/Code/2/2.1/2-float/main.c.obj.d"
+  "CMakeFiles/symbolic_constants.dir/main.c.obj"
+  "CMakeFiles/symbolic_constants.dir/main.c.obj.d"
+  "libsymbolic_constants.dll.a"
+  "symbolic_constants.exe"
+  "symbolic_constants.exe.manifest"
+  "symbolic_constants.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/symbolic_constants.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
